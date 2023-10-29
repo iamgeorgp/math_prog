4de638b8aff9bc2c5356a7ada13506270e262dac
iamgeorgp <plus.george@yandex.ru>
2023-10-29 19:09:51 +0300

    upd. formatting and extremum methods

--- extremum/f_method.py
+++ extremum/f_method.py
@@ -1,10 +1,11 @@
+#-------------------------------------------------------------------------------------------------------
 import sympy
+import scipy
 
 #--------------------------------------------------------------------------------------------------------
 def gradient_method_step_split(f, x, variables, alpha, beta, epsilon):
 	# def gradient of function
 	grad_f = [sympy.diff(f, var) for var in variables]
-	
 	# iteration of method
 	iteration = 0
 	x1 = variables[0]
@@ -22,7 +23,26 @@ def gradient_method_step_split(f, x, variables, alpha, beta, epsilon):
 		x = x_new
 		iteration += 1
 	return iteration, x
-#--------------------------------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------------------------------
+def gradient_descent(f, x, variables, tol=1e-5):
+    iteration = 0
+    x1,x2,x3 = variables
+    grad_f = [sympy.diff(f, var) for var in variables]
+    while True:
+        grad = [g.evalf(subs={x1: x[0], x2: x[1], x3: x[2]}) for g in grad_f]
+        lambda_min = sympy.symbols('lambda')
+        f_lambda = f.subs({x1: x[0] - lambda_min*grad[0], 
+                           x2: x[1] - lambda_min*grad[1], 
+                           x3: x[2] - lambda_min*grad[2]})
+        lambda_min = sympy.solve(sympy.diff(f_lambda, lambda_min))[0]
+        x = [x[j] - lambda_min*grad[j] for j in range(len(x))]
+        if all(abs(g) < tol for g in grad):
+            break
+        iteration += 1
+    return x, iteration
+
+#-------------------------------------------------------------------------------------------------------
 def newton_method(f, x, variables, epsilon=0.001):
     # def gradient and Hesse matrix
     grad_f = [sympy.diff(f, var) for var in variables]
@@ -31,66 +51,53 @@ def newton_method(f, x, variables, epsilon=0.001):
     x_current = sympy.Matrix(x)
     iteration = 0
     while True:
+        iteration += 1
         gradient_value = [grad.subs({var: val for var, val in zip(variables, x_current)}) for grad in grad_f]
         Hessian_value = Hessian_matrix.subs({var: val for var, val in zip(variables, x_current)})
         delta_x = Hessian_value.inv() * sympy.Matrix(gradient_value)
         x_new = x_current - delta_x
         norm_delta_x = sympy.sqrt(sum(val**2 for val in delta_x))
-
         if norm_delta_x < epsilon:
             break
-
         x_current = x_new
-        iteration += 1
     x_result = [float(each) for each in x_current]
     return x_result, iteration
-#--------------------------------------------------------------------------------------------------------
-def fast_descent(f, x, variables, tolerance=1e-6, epsilon=0.001):
-	grad_f = [sympy.diff(f, var) for var in variables]
-	x1 = variables[0]
-	x2 = variables[1]
-	x3 = variables[2]
-	x_values = {x1: x[0], x2: x[1], x3: x[2]}
-	iteration=0
-	while True:
-		gradient_at_point = [grad.subs(x_values) for grad in grad_f]
-		
-		if all(abs(gradient) < tolerance for gradient in gradient_at_point):
-			break
-		
-		for var, grad_value in zip((x1, x2, x3), gradient_at_point):
-			x_values[var] -= epsilon * grad_value
-		iteration +=1
 
-	x_result = [x_values[var] for var in variables]
-	return x_result, iteration
-#--------------------------------------------------------------------------------------------------------
-def coordinate_descent(f, x, variables, epsilon, precision=1e-6):
-    x1_val = x[0]; x1 = variables[0];
-    x2_val = x[1]; x2 = variables[1];
-    x3_val = x[2]; x3 = variables[2];
+#-------------------------------------------------------------------------------------------------------
+def conjugate_gradient_method(f, x, variables):
+    x1 = variables[0]
+    x2 = variables[1]
+    x3 = variables[2]
+    def func(x):
+        return f.subs({x1: x[0], 
+					   x2: x[1], 
+					   x3: x[2]})
+
+    res = scipy.optimize.minimize(
+				   func, 
+                   x, 
+                   method='CG', 
+                   options={'disp': False})
+    x_point = res.x
+    iteration = res.nit
+    return x_point, iteration
+
+#-------------------------------------------------------------------------------------------------------
+def coordinate_descent_method(f, x0, variables, tolerance = 1e-6):
+    x1 = variables[0]
+    x2 = variables[1]
+    x3 = variables[2]
     iteration = 0
     while True:
-        if iteration > 0:
-            delta_x1 = abs(x1_val - x1_prev)
-            delta_x2 = abs(x2_val - x2_prev)
-            delta_x3 = abs(x3_val - x3_prev)
-            if delta_x1 < precision and delta_x2 < precision and delta_x3 < precision:
-                break
-        
-        x1_prev = x1_val
-        x2_prev = x2_val
-        x3_prev = x3_val
-        
-        df_dx1 = sympy.diff(f, x1).subs({x1: x1_val, x2: x2_val, x3: x3_val})
-        df_dx2 = sympy.diff(f, x2).subs({x1: x1_val, x2: x2_val, x3: x3_val})
-        df_dx3 = sympy.diff(f, x3).subs({x1: x1_val, x2: x2_val, x3: x3_val})
-        
-        x1_val -= epsilon * df_dx1
-        x2_val -= epsilon * df_dx2
-        x3_val -= epsilon * df_dx3
-        
-        iteration += 1
-    x_result=[x1_val, x2_val, x3_val]
-    return x_result, iteration
+        f_prev = f.subs({xj: x0[j] for j, xj in enumerate([x1, x2, x3])})
+        for i, xi in enumerate([x1, x2, x3]):
+            f_xi = f.subs({xj: x0[j] for j, xj in enumerate([x1, x2, x3]) if j != i})
+            result = scipy.optimize.minimize_scalar(lambda x: float(f_xi.subs(xi, x)), method='golden')  # Преобразуем результат к числу
+            x0[i] = result.x
+        f_current = f.subs({xj: x0[j] for j, xj in enumerate([x1, x2, x3])})
+        iteration +=1
+        if abs(f_current - f_prev) < tolerance:
+            break
+    return x0, iteration
 
+#-------------------------------------------------------------------------------------------------------
